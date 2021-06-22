380a81e181612c3ae9eed64aceae25d42576e916
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-22 20:49:28 +0300

    Add model class for subscription

--- app/models.py
+++ app/models.py
@@ -64,4 +64,14 @@ class Note(db.Model):
 
   def __repr__(self):
     return f'note {self.title}'
+class Subscriber(db.Model):
+  __tablename__='subscribers'
+  id=db.Column(db.Integer,primary_key=True)
+  email = db.Column(db.String(255),unique=True,index=True)
 
+  def save_subscriber(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return f'Subscriber {self.email}'
