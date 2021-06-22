d71d048c72be914cfb491c305f0ff82bce8db488
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-22 19:11:44 +0300

    Add a route view for notes

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template, redirect,url_for,abort,request
+from . import main
+from flask_login import login_required,current_user
+from ..models import User,note,Comment,Subscriber
+from ..import db, photos
+import secrets
+import os
+from PIL import Image
+from .forms import UpdateProfile,Createnote
+from ..email import mail_message
+
+#Views
+@main.route('/')
+def index():
+    notes = note.query.order_by(note.time.desc())
+    return render_template('notes_page.html', notes=notes)
